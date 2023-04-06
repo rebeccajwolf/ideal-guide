24d4b40cfc489e7a531a17ab17e95590c3b027f0
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2023-04-06 11:28:51 -0700

    Update Files

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 nohup gunicorn keep_alive:app --bind 0.0.0.0:8080 &
 
-python3 ms_rewards_farmer.py --error --session --fast --headless --telegram "${TOKEN}" "${CHAT_ID}" ;
+python3 ms_rewards_farmer.py --shuffle --dont-check-internet --virtual-display --repeat-shopping --dont-check-for-updates --no-images --error --session --fast --headless --telegram "${TOKEN}" "${CHAT_ID}" ;
 
-python3 ms_rewards_farmer.py --error --everyday --start-at 04:00 --session --fast --headless --telegram "${TOKEN}" "${CHAT_ID}"
+python3 ms_rewards_farmer.py --shuffle --dont-check-internet --virtual-display --repeat-shopping --dont-check-for-updates --no-images --error --everyday --start-at 04:00 --session --fast --headless --telegram "${TOKEN}" "${CHAT_ID}"
 
